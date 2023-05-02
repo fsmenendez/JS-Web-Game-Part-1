ce96b24b2c5407e1c889e838590c5d88fdbd3f83
franc <fmenendezwork@gmail.com>
2023-05-02 15:13:18 -0700

    fixed broken function, added new objects

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom ='250px'
-document.body.append(pineTree)
-
-let newImage = (src, left, bottom) => {
-    gameObject.src = src
-    gameObject.style.position = left
-    gameObject.style.left = bottom
-    gameObject.style.bottom
+function newImage(link, left, bottom){
+    let gameObject = document.createElement('img')
+    gameObject.src = link
+    gameObject.style.position = 'fixed'
+    gameObject.style.left = left + 'px'
+    gameObject.style.bottom = bottom + 'px'
     document.body.append(gameObject)
 }
 
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
