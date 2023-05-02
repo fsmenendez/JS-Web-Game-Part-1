fc8b69fd19fb4801ac8d7e1a3c7cbc54f2c7386b
franc <fmenendezwork@gmail.com>
2023-05-02 15:28:49 -0700

    created function newItem

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+function newItem(link, left, bottom){
+    let gameItem = document.createElement('img')
+    gameItem.src = link
+    gameItem.style.position = 'fixed'
+    gameItem.style.left = left + 'px'
+    gameItem.style.bottom = bottom + 'px'
+    document.body.append(gameItem)
+    gameItem.addEventListener('click', function(){
+        gameItem.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
